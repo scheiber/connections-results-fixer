616edf181bbf75fa5ca643b92f711ad6cb5d535a
Jonathan Scheiber <jonathan@scheiber.dev>
2024-06-13 00:16:02 -0400

    continue rebrand and refactor

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
-document
-  .getElementById("inputText")
-  .addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      fixConnections();
-    }
-  });
+const inputTextElement = document.getElementById("inputText");
+const outputMessageElement = document.getElementById("outputMessage");
 
-const fixConnections = () => {
-  let input = document.getElementById("inputText").value.trim();
+const handleKeyDown = (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    fixResults();
+  }
+};
+
+inputTextElement.addEventListener("keydown", handleKeyDown);
+
+const fixResults = () => {
+  let input = inputTextElement.value.trim();
   if (input === "") {
-    document.getElementById("outputMessage").textContent =
-      "Please provide a result to be fixed.";
+    outputMessageElement.textContent = "Please provide a result to be fixed.";
     return;
   }
   let output = addSpacesToLineBreaks(input);
   copyToClipboard(output);
-  document.getElementById("outputMessage").textContent =
+  outputMessageElement.textContent =
     "Success! The fixed result has been copied to your clipboard.";
   resetFixer();
 };
 
 const resetFixer = () => {
-  document.getElementById("inputText").value = "";
+  inputTextElement.value = "";
   setTimeout(() => {
-    document.getElementById("outputMessage").textContent = "";
+    outputMessageElement.textContent = "";
   }, 5000);
 };
 
 const addSpacesToLineBreaks = (input) => {
-  let lines = input.split("\n");
-  lines = lines.filter((line) => line.trim() !== "");
-  for (let i = 0; i < lines.length; i++) {
-    if (i === 0 && lines[i].endsWith(" ")) {
-      lines[i] += " ";
-    } else {
-      lines[i] += "  ";
-    }
-  }
+  let lines = input.split("\n").filter((line) => line.trim() !== "");
+  lines = lines.map((line, index) =>
+    index === 0 && line.endsWith(" ") ? line + " " : line + "  "
+  );
   return lines.join("\n") + `\n\n`;
 };
 
-const copyToClipboard = (text) => {
-  navigator.clipboard.writeText(text).catch((err) => {
+const copyToClipboard = async (text) => {
+  try {
+    await navigator.clipboard.writeText(text);
+  } catch (err) {
     console.error("Failed to copy: ", err);
-  });
+    outputMessageElement.textContent =
+      "Failed to copy to clipboard. Please try again.";
+  }
 };
